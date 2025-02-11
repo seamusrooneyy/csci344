667c3eef1a54f38b89211d79da515403bba81763
seamusrooneyy <seamusrooneyy@gmail.com>
2025-02-11 13:17:17 -0500

    Completed Tutorial 3!

--- tutorials/tutorial03/tailwind.config.js
+++ tutorials/tutorial03/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./*html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-blue": "#00BAFF",
+        "custom-purple": "#6336FA",
+      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins"],
+    }
   },
   plugins: [],
 }
